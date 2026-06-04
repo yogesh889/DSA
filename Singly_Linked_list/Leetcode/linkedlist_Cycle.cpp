c052054edc271e6d8ad5fa9430894aaec3735a91
Yogesh889 <8058yogesh@gmail.com>
2026-06-04 16:11:43 +0530

    leetcode linkedlist

--- Singly_Linked_list/Leetcode/linkedlist_Cycle.cpp
+++ Singly_Linked_list/Leetcode/linkedlist_Cycle.cpp
@@ -9,9 +9,45 @@ class Node{
             val = data;
             next = nullptr;
         }
+};
+
+void insert_At_Head(Node* &head, Node* &tail, int x){
+    Node* newNode = new Node(x);
+    if(head == nullptr){
+        head = tail = newNode;
+        return;
+    }
+    newNode->next = head;
+    head = newNode;
+}
+
+int print(Node* head){
+    Node* temp = head;
+
+    int cnt = 0;
+    while(temp){
+        cout << temp->val << " -> ";
+        temp = temp->next;
+        cnt++;
+    }
+
+    cout << "NULL"<<endl;
+    return cnt;
 }
 
 int main(){
+
+    Node* head = nullptr;
+    Node* tail = nullptr;
+
+    insert_At_Head(head, tail, 10);
+    insert_At_Head(head, tail, 20);
+    insert_At_Head(head, tail, 30);
+    insert_At_Head(head, tail, 40);
+    insert_At_Head(head, tail, 50);
+    insert_At_Head(head, tail, 60);
+    insert_At_Head(head, tail, 70);
+    int count = print(head);
 
     return 0;
 }
